836f9e86ab26d952ce7bb24f693052af481c1f7c
Nayra-pixel <nayrasduarte@gmail.com>
2025-06-02 19:48:21 -0300

    first commit

---
+++ forms.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Formulário</title>
+	<link rel="stylesheet" type="text/css" href="ccs/style.css">
+</head>
+<body>
+	<h1>Formulário PHP</h1>
+	<form action="#" method="GET">
+		Nome:
+		<input type="text" name="nome">
+		<br>
+		E-mail:
+		<input type="email" name="email">
+		<br>
+		<input type="submit" name="enviar" value="Acessar o Sistema">
+		
+		<input type="reset" name="limpar" value="Limpar">
+
+	</form>
+</body>
+</html>
